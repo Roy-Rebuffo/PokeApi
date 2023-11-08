76113f7edda13d8a4cbed4105963163a438dc9b4
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2023-11-08 17:20:49 +0100

    arreglo de recuperar los primeros 150 pokemons

--- pokedex.js
+++ pokedex.js
@@ -1,27 +1,38 @@
 //Funcion que inicia y recoge los 150 primeros pokemon
-const pokeApi = async() => {
-    const pokeList$$ = document.querySelector('#pokedex');
-    const pokeList = [];
+const pokeApi = async () => {
+    const pokeList$$ = document.querySelector("#pokedex");
+    const baseUrl$$ = "https://pokeapi.co/api/v2/pokemon/";
+    const pokemonList = [];
   
-    for(let i =1; i<=150; i++){
-      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
-      const res = await response.json();
-      console.log(res)
+    for (let i = 1; i <= 150; i++) {
+      const pokemonUrl = baseUrl$$ + i;
+      const response = await fetch(pokemonUrl);
+      const result = await response.json();
+      
+      console.log(result.name);
   
-      //MAPEAR LOS POKEMONS 
+      // MAPEAR LOS POKEMONS
+      const pokemon = {
+          name: result.name,
+          image: result.sprites['front_default'],
+          type: result.types.map((type) => type.type.name).join(', '),
+          id: result.id,
+      };
+  
+      pokemonList.push(pokemon);
     }
-  }
-  //Esta va a ser mi funcion para ordenar mis otras funciones. Porque solo me devuelve 20?
-  const startApi = async() => {
   
+    return pokemonList;
+  };
+  
+  // Esta va a ser mi función para ordenar mis otras funciones.
+  const startApi = async () => {
     const pokemons = await pokeApi();
     console.log(pokemons);
   };
-  startApi();
   
-  
-  function paintPokemons(){
-  
+
+  const panitPokemons = () => {
   }
-  
-  
+
+  startApi();
