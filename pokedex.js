91dad72580397748ddd3f1185de0d7baeb14eb3e
Roy Rebuffo <roy.rebuffo@bootcamp-upgrade.com>
2023-11-10 11:57:18 +0100

    arreglo del mapeo + puesto fuera de la funcion pokeApi para que no me salga undefined

--- pokedex.js
+++ pokedex.js
@@ -1,64 +1,40 @@
 //THIS FUNCTION INITIALICES AN API CALLING THE POKEMONS FROM 1 TO 150
 const pokeApi = async () => {
-    const pokeList$$ = document.querySelector("#pokedex");
-    const baseUrl$$ = "https://pokeapi.co/api/v2/pokemon/";
-    const pokemonList = [];
-  
-    for (let i = 1; i <= 150; i++) {
-      const pokemonUrl = baseUrl$$ + i;
-      const response = await fetch(pokemonUrl);
-      const result = await response.json();
-      
-      //con un return solo me devuelve bulbasaur
-      console.log(result.name);
-      
-  
-      // MAP POKEMONS
+  const pokeList$$ = document.querySelector("#pokedex");
+  const baseUrl$$ = "https://pokeapi.co/api/v2/pokemon/";
+  const pokemonList = [];
 
-      const pokemon = {
-        
-          name: result.name,
-          image: result.sprites['front_default'],
-          type: result.types.map((type) => type.type.name).join(', '),
-          id: result.id,
-      };
-  
-      pokemonList.push(pokemon);
-    }
-    
-    return pokemonList;
-  };
+  for (let i = 1; i <= 150; i++) {
+    const pokemonUrl = baseUrl$$ + i;
+    const response = await fetch(pokemonUrl);
+    const result = await response.json();
 
-
-  //THIS IS GOING TO BE MY FUNCTION THAT IS GOING TO ORDER MY CODE
-
-
-  // esta funcion me da error y desconozco el motivo
-  
-  const mappedPokemons = (pokemonCharacters) => {
-    const mappedPokemonData = pokemonCharacters.map((character) => ({
-      name: character.name,
-      image: character.sprites['front_default'],
-      type: character.types.map((type) => type.type.name).join(', '),
-      id: character.id,
-    }));
-    return mappedPokemonData;
-  };
-  
-
-  const startApi = async () => {
-    const pokemons = await pokeApi();
-    console.log(pokemons);
-    //esta funcion para mapear me da error y desconozco el motivo
-    const mappedPokemonsData = mappedPokemons(pokemons);
-    console.log(mappedPokemonsData);
-    //
-    paintPokemons();
-
-  };
-  
-
-  const paintPokemons = () => {
+    pokemonList.push(mappedPokemons(result));
   }
 
-  startApi();
+  return pokemonList;
+};
+// MAPPED POKEMONS
+const mappedPokemons = (result) => ({
+  name: result.name,
+  image: result.sprites.front_default,
+  type: result.types.map((type) => type.type.name).join(', '),
+  id: result.id,
+});
+
+//THIS IS GOING TO BE MY FUNCTION THAT IS GOING TO ORDER MY CODE
+const startApi = async () => {
+  const pokemons = await pokeApi();
+  console.log(pokemons);
+
+  const mappedPokemonsData = pokemons.map(mappedPokemons);
+  console.log(mappedPokemonsData);
+
+  paintPokemons();
+};
+
+const paintPokemons = () => {
+  
+};
+
+startApi();
